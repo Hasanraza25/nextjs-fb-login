82df16c213d682f1e029e31b5474361eb0db004c
Hasan Raza <hasanhussain2580@gmail.com>
2024-09-22 19:04:22 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,45 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="bg-gray-100 h-screen flex flex-col lg:flex-row md:flex-col sm:flex-col items-center justify-center xs:mb-28 sm:mb-28 lg:mb-0">
+      <div className="text-3xl lg:w-1/2 lg:-mt-40 mt-60 w-full sm:ml-8 xs:pe-5">
         <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
+          src={"images/facebook.svg"}
+          width={300}
+          height={100}
+          alt="Facebook Logo"
+          className="w-80 lg:m-0"
         />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <p className="ml-7 -mt-1 f-size p-color xl:pe-24 lg:pe-20">
+          Facebook helps you connect and share with the people in your life.
+        </p>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+      <div className="bg-white flex flex-col p-4 rounded-lg shadow-xl mb-32  lg:-ml-12 lg:w-1/3 sm:w-11/12 xs:w-11/12 sm:mt-10 xs:mt-10">
+        <input
+          className="my-2 border border-1 border-gray-200 p-3 pl-4 placeholder:text-base rounded-lg myOutline focus:text-black active:text-gray-300 focus:ring-1 placeholder:text-inputColor"
+          autoFocus
+          type="text"
+          placeholder="Email address or phone number"
+        />
+        <input
+          className="my-2 border border-1 border-gray-200 p-3 pl-4 placeholder:text-base rounded-lg myOutline focus:text-black active:text-gray-300 focus:ring-1 placeholder:text-inputColor"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="bg-blueBg hover:bg-hoverBlue text-white p-2.5 mt-2 font-bold text-xl rounded-lg">
+          Log in
+        </button>
+        <a href="#" className="m-auto my-4 text-sm text-blueBg hover:underline">
+          Forgotten password?
         </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <span>
+          <hr />
+        </span>
+        <button className="bg-myGreen hover:bg-hoverGreen p-3 text-white rounded-lg font-bold text-lg m-auto mt-5 mb-2">
+          Create new account
+        </button>
+      </div>
     </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      screens: {
+        'xs': {'max': '639px'}, // Custom max-width breakpoint
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        myGreen: '#42b72a',
+        hoverGreen: '#36a420',
+        blueBg: '#0866ff',
+        hoverBlue: '#1877f2',
+        inputColor: '#9e9fa3',
       },
     },
   },
